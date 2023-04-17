691c01e73dc8b113e0e5cd6293fdd43b2aac1a98
Memo Cachú <gcachu.o@gmail.com>
2023-04-17 11:25:30 -0600

    add calendar screen

--- screens/CalendarScreen.tsx
+++ screens/CalendarScreen.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import moment from "moment";
-import { authorize } from "react-native-app-auth";
 import { Calendar as CalendarAPI } from "react-native-big-calendar";
 
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
@@ -22,17 +21,6 @@ const CalendarScreen = () => {
           revocationEndpoint: "https://oauth2.googleapis.com/revoke",
         },
       };
-
-      const authResult = await authorize(config);
-
-      // const { data } = await calendar.events.list({
-      //   auth: authResult.accessToken,
-      //   calendarId: "primary",
-      //   timeMin: new Date().toISOString(),
-      //   maxResults: 10,
-      //   singleEvents: true,
-      //   orderBy: "startTime",
-      // });
 
       const events = ([] as any).map((event) => {
         const start = moment(event.start?.dateTime || event.start?.date);
